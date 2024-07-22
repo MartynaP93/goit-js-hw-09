c6880b6bcdab3aa9dee8bd80296ad284a9fa0cfd
MartynaP93 <martyna.anna.piotrowska@gmail.com>
2024-07-22 12:38:44 +0200

    modul9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+let colorTimeout
 
+function getRandomHexColor() {  
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
+    
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+    colorTimeout = setTimeout(changeBackgroundColor, 1000)
+}
+
+document.getElementById("startButton").addEventListener('click', function() {
+    this.disable = true;
+    document.getElementById("stopButton").disable = false;
+    changeBackgroundColor();
+})
+   
+document.getElementById("stopButton").addEventListener('click', function() {
+    this.disable = true;
+    document.getElementById("startButton").disable = false;
+    clearTimeout(colorTimeout)
+})
